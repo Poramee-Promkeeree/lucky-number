6b559be2491e8a2415d359a889a972ce0f055bd5
นิธิกร ปานสังข์ <pansang1223@gmail.com>
2023-03-03 14:16:30 +0700

    สุ่มตัวเลข1-100

--- game-server.py
+++ game-server.py
@@ -14,6 +14,16 @@ class ClientThread(threading.Thread):
         threading.Thread.__init__(self)
         self.conn = conn
         self.addr = addr
+
+        def run(self):
+        print('Connected by', self.addr)
+
+        # send the answer range to the client
+        self.conn.sendall(f"Guess an integer between 1 and 100 inclusive. You have 10 attempts.".encode())
+
+        # generate a random number between 1 and 100 for this client
+        answer = random.randint(1, 100)
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
